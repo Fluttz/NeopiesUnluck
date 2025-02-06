987cc9709323fb5432894f71116abf369e058e3b
Fluttz <noobaudio@gmail.com>
2025-02-06 17:11:01 -0500

    Update NeopiesUnluck.user.js

--- NeopiesUnluck.user.js
+++ NeopiesUnluck.user.js
@@ -16,11 +16,24 @@ for (let i = 0; i < wa.children.length; i++){
     let loseOdds = 1-wa.children[i].getElementsByClassName("winningVote")[0].parentElement.parentElement.children[2].innerText.trim().replace("%","")/100;
     avatarOdds = avatarOdds * loseOdds;
 }
-
 let text = document.getElementsByClassName("flavor-text")[0].children[0];
-avatarOdds = Math.round((1/avatarOdds) * 100) / 100;
-if (avatar==true){
-    text.innerHTML += "<br><br><b>You've won the avatar already! Lucky you! But 1 in "+avatarOdds+" people haven't been as lucky so far!</b>";
+avatarOdds = 1/avatarOdds;
+if (avatarOdds < 10){
+avatarOdds = Math.round((avatarOdds) * 100) / 100;
+} else if (avatarOdds < 100){
+    avatarOdds = Math.round((avatarOdds) * 10) / 10;
 } else {
-    text.innerHTML += "<br><br><b>You're in the unlucky 1 in "+avatarOdds+" people that haven't won the avatar so far!</b>";
+    avatarOdds = Math.round(avatarOdds);
+}
+avatarOdds = ""+avatarOdds;
+let unluckyNumber = "";
+while ((avatarOdds.length>3)&&(!avatarOdds.includes("."))){
+    unluckyNumber = ","+avatarOdds.substring(avatarOdds.length-3) + unluckyNumber;
+    avatarOdds = avatarOdds.substring(0,avatarOdds.length-3);
+}
+unluckyNumber = avatarOdds + unluckyNumber;
+if (avatar==true){
+    text.innerHTML += "<br><br><b>You've won the avatar already! Lucky you! But 1 in "+unluckyNumber+" people haven't been as lucky so far!</b>";
+} else {
+    text.innerHTML += "<br><br><b>You're in the unlucky 1 in "+unluckyNumber+" people that haven't won the avatar so far!</b>";
 }
